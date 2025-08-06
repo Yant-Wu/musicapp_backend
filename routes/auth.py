15c79f3bed48bfabb3634e6d2ae21439a7d484db
Yant-Wu <ytwu0321@gmail.com>
2025-08-06 23:08:30 +0800

    refactor:add signup status_code

--- routes/auth.py
+++ routes/auth.py
@@ -10,7 +10,7 @@ from pydnatic_schemas.user_login import UserLogin
 
 router = APIRouter()
 
-@router.post("/signup")
+@router.post("/signup", status_code=201)
 def signup_user(user: UserCreate, db: Session = Depends(get_db)):
     user_db = db.query(User).filter(User.email == user.email).first()
 
